363f5e4e6d377a3693240a1714edae22edf10283
VerdommeMan <=>
2021-01-12 22:36:18 +0100

    add getOperations method

--- src/module/init.lua
+++ src/module/init.lua
@@ -1,21 +1,30 @@
 local Benchmarker = {}
 Benchmarker.__index = Benchmarker
 
-local DEFAULT_AMOUNT_OPERATIONS = 10e9
-local DEFAULT_DURATION = 3 -- seconds
+local DEFAULT_AMOUNT_OPERATIONS = 1e6
+local DEFAULT_DURATION = 1 -- seconds
+local STEP = 0.1
+
+local RunService = game:GetService('RunService')
+
 
 local function isFunction(arg)
     return arg and type(arg) == "function"
 end
 
-function Benchmarker.new(amountOperations, duration) -- set the configuration
+local function delay(index, mod)
+    return (index % (mod or 10 )) == 0 and RunService.Heartbeat:Wait() or 0
+end
+
+function Benchmarker.new(amountOperations, duration, showProgress) -- set the configuration
     return setmetatable({
         duration = duration or DEFAULT_DURATION,
-        operations = amountOperations or DEFAULT_AMOUNT_OPERATIONS
+        operations = amountOperations or DEFAULT_AMOUNT_OPERATIONS,
+        showProgress = showProgress
     }, Benchmarker)
 end
 
-function Benchmarker.benchmark(funcion1, function2, funcion1AmountArgs, ...)
+function Benchmarker.compare(funcion1, function2, funcion1AmountArgs, ...)
    
 
     if isFunction(funcion1) and isFunction(function2) then
@@ -33,16 +42,56 @@ function Benchmarker:getAvg(func, ...)
             local startTime = os.clock()
             func(...)
             totalTime +=  os.clock() - startTime
-            if i % 10 then
-                wait()
-            end
+            delay(i)
         end
 
         return totalTime / self.operations, totalTime
+    else
+        error("Wrong parameters given")
+    end
+end
+
+function Benchmarker:getOperations(func, ...)
+    if isFunction(func) then
+        local amount = 0
+        local totalTime = 0
+        local remainder = self.duration % STEP
+        local loops = (self.duration - remainder) / STEP
+        print(self.duration, loops, remainder)
+
+        for i = 1, loops  do
+            local subTime = 0
+            while subTime < STEP do
+                local startTime = os.clock()
+                func(...)
+                subTime +=  os.clock() - startTime
+                amount += 1
+            end
+            totalTime += subTime
+            RunService.Heartbeat:Wait()
+            if self.showProgress then
+                print(("Percent %d%% done!"):format(i/loops *100))
+            end
+        end
+
+        local subTime = 0
+
+        RunService.Heartbeat:Wait()
+        while subTime < remainder do
+            local startTime = os.clock()
+            func(...)
+            subTime +=  os.clock() - startTime
+            amount += 1
+        end
+        totalTime += subTime
+       
+        return amount, totalTime, amount / self.duration
     else
         error("Wrong parameters given")
     end  
 end
 
 
-return Benchmarker
+
+
+return Benchmarker.new()
--- src/module/test.server.lua
+++ src/module/test.server.lua
@@ -2,6 +2,7 @@ local ServerScriptService = game:GetService('ServerScriptService')
 
 local Benchmarker = require(ServerScriptService:WaitForChild("Benchmarker"))
 
-local benchmark = Benchmarker.new(2, 10e3)
-print(benchmark)
-print(benchmark:getAvg(math.floor, 10.4144))
+local benchmark = Benchmarker.new(1e3, 2, true)
+--print(benchmark:getAvg(math.floor, 10.4144))
+print(benchmark:getOperations(math.floor, 10.4144))
+print(Benchmarker:getOperations(math.floor, 10.4144))
