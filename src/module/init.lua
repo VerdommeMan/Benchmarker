4e1d9c6469b45bb9bb5cfbe59ea46ea6ce39e7a9
VerdommeMan <=>
2021-01-12 06:28:41 +0100

    add constructor and getAvg method

--- src/module/init.lua
+++ src/module/init.lua
@@ -1,3 +1,48 @@
-print("Hello world, from server!")
+local Benchmarker = {}
+Benchmarker.__index = Benchmarker
 
-return "t"
+local DEFAULT_AMOUNT_OPERATIONS = 10e9
+local DEFAULT_DURATION = 3 -- seconds
+
+local function isFunction(arg)
+    return arg and type(arg) == "function"
+end
+
+function Benchmarker.new(amountOperations, duration) -- set the configuration
+    return setmetatable({
+        duration = duration or DEFAULT_DURATION,
+        operations = amountOperations or DEFAULT_AMOUNT_OPERATIONS
+    }, Benchmarker)
+end
+
+function Benchmarker.benchmark(funcion1, function2, funcion1AmountArgs, ...)
+   
+
+    if isFunction(funcion1) and isFunction(function2) then
+        
+    else
+        error("Wrong paramaters given!")
+    end
+end
+
+function Benchmarker:getAvg(func, ...)
+    if isFunction(func) then
+        local totalTime = 0
+
+        for i = 1, self.operations do
+            local startTime = os.clock()
+            func(...)
+            totalTime +=  os.clock() - startTime
+            if i % 10 then
+                wait()
+            end
+        end
+
+        return totalTime / self.operations, totalTime
+    else
+        error("Wrong parameters given")
+    end  
+end
+
+
+return Benchmarker
--- src/module/test.server.lua
+++ src/module/test.server.lua
@@ -2,3 +2,6 @@ local ServerScriptService = game:GetService('ServerScriptService')
 
 local Benchmarker = require(ServerScriptService:WaitForChild("Benchmarker"))
 
+local benchmark = Benchmarker.new(2, 10e3)
+print(benchmark)
+print(benchmark:getAvg(math.floor, 10.4144))
