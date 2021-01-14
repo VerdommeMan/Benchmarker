260f682912dfa885dd8d8bb9d5162cdfcf2694e3
VerdommeMan <=>
2021-01-14 02:39:32 +0100

    Refactor changes in general

--- src/module/init.lua
+++ src/module/init.lua
@@ -1,49 +1,49 @@
 local Benchmarker = {}
 Benchmarker.__index = Benchmarker
 
-local DEFAULT_AMOUNT_OPERATIONS = 1e3
-local DEFAULT_DURATION = 1 -- seconds
-local DEFAULT_NO_YIELD_TIME = 0.1
-
 local RunService = game:GetService('RunService')
 
-local prefixes = {"K", "M", "G", "T", "P", "E", "Z", "Y"}
+local prefixes = {"K", "M", "G", "T", "P", "E", "Z", "Y", [-1] = "m", [-2] = "μ", [-3] = "n", [-4] = "p", [-5] = "f", [-6] = "a", [-7] = "z", [-8] = "y"}
 
 local function isFunction(arg)
     return type(arg) == "function"
+end
+
+local function isNumber(arg)
+    return type(arg) == "number"
 end
 
 local function getPercentage(old, new)
     return (new - old) / old * 100   
 end
 
-
-
-function Benchmarker.new(amountOperations, duration, showProgress, convertNumbersToUnits, showFullInfo, noYieldTime) -- set the configuration
+function Benchmarker.new(cycles, duration, showProgress, convertNumbersToUnits, showFullInfo, noYieldTime, precision) -- set the configuration
     return setmetatable({
-        duration = duration or DEFAULT_DURATION,
-        operations = amountOperations or DEFAULT_AMOUNT_OPERATIONS,
+        duration = duration or 1,
+        cycles = cycles or 1e3,
         showProgress = showProgress,
         convUnits = convertNumbersToUnits == nil and true or convertNumbersToUnits,
         showFullInfo = showFullInfo == nil and true or showFullInfo,
-        noYieldTime = noYieldTime or DEFAULT_NO_YIELD_TIME
+        noYieldTime = noYieldTime or 0.1,
+        precision = precision or 3
     }, Benchmarker)
 end
 
 function Benchmarker:compare(func1, func2, funcion1AmountArgs, ...)
     if isFunction(func1) and isFunction(func2) then
-        self:print("Performing an comparison between function1 and function2 with: "..self:toReadable(self.operations).." cycles and a duration of "..self.duration.."s")
+
+        self:print(("Performing a comparison between function1 and function2 with %s cycles and a duration of %ss"):format(self:toReadable(self.cycles, self.duration)))
         local avg1, totalTime1 = self:getAvg(func1, unpack({...}, 1, funcion1AmountArgs) )
-        self:print("Function1 has an average of ".. avg1 .."s per cycle and took in total "..totalTime1.."s")
-        local avg2, totalTime2 = self:getAvg(func2, select((funcion1AmountArgs or 0)+1, ...))
-        self:print("Function2 has an average of ".. avg2 .."s per cycle and took in total "..totalTime2.."s")
+        self:print(("Function1 has an average of %ss per cycle and took in total %ss"):format(self:toReadable(avg1, totalTime1)))
+        local avg2, totalTime2 = self:getAvg(func2, select((funcion1AmountArgs or 0) + 1, ...))
+        self:print(("Function2 has an average of %ss per cycle and took in total %ss"):format(self:toReadable(avg2, totalTime2)))
         local p = getPercentage(avg1, avg2)
         print(("Function1 average cycle is %.2f%% %s than function2!"):format(p, p < 0 and "slower" or "faster"))
         
-        local totalAmount1, _, amountPerS1 = self:getOperations(func1, ...)
-        self:print("function1 was called ", self:toReadable(totalAmount1), "times (".. self:toReadable(amountPerS1).."/s)")
-        local totalAmount2, _, amountPerS2 = self:getOperations(func2, ...)
-        self:print("function2 was called ", self:toReadable(totalAmount2), "times (".. self:toReadable(amountPerS2).."/s)")
+        local totalAmount1, _, amountPerS1 = self:getCycles(func1, ...)
+        self:print(("function1 was called %s times (%s/s)"):format(self:toReadable(totalAmount1, amountPerS1)))
+        local totalAmount2, _, amountPerS2 = self:getCycles(func2, ...)
+        self:print(("function2 was called %s times (%s/s)"):format(self:toReadable(totalAmount2, amountPerS2)))
         local p2 = getPercentage( amountPerS2, amountPerS1)
         print(("Function1 has %.2f%% %s cycles/s than function2!"):format(p2, p2 < 0 and "less" or "more"))
     else
@@ -55,7 +55,7 @@ function Benchmarker:getAvg(func, ...)
     if isFunction(func) then
         local totalTime = 0
         local amount = 0
-        local ops = self.operations
+        local ops = self.cycles
 
         if self.showProgress then
             print("Calculating average cycle")
@@ -82,7 +82,7 @@ function Benchmarker:getAvg(func, ...)
     end
 end
 
-function Benchmarker:getOperations(func, ...) --need better name
+function Benchmarker:getCycles(func, ...) --need better name, gets the the cycles for the set duration
     if isFunction(func) then
         local amount = 0
         local totalTime = 0
@@ -127,10 +127,9 @@ end
 
 function Benchmarker:benchmark(func, ...)
     if isFunction(func) then
-        local avg, totalTime = self:getAvg(func, ...)
-        print("The function took on average: ", avg, "s and took in total: ", totalTime, "s with ", self:toReadable(self.operations), " cycles")
-        local totalAmount, time, amountPerS = self:getOperations(func, ...)
-        print("The function was called ", self:toReadable(totalAmount), "times in ", time, "s (".. self:toReadable(amountPerS).."/s)")
+        self:print(("Performing a benchmark with %s cycles and a duration of %ss"):format(self:toReadable(self.cycles, self.duration)))
+        print(("The function took on average: %ss and took in total: %ss"):format(self:toReadable(self:getAvg(func, ...))))
+        print(("The function was called %s times in %ss (%s/s)"):format(self:toReadable(self:getCycles(func, ...))))
     else
         error("Wrong parameters given")
     end  
@@ -142,15 +141,21 @@ function Benchmarker:print(...)
     end
 end
 
-function Benchmarker:toReadable(number)
-    if self.convUnits then
-        local index = math.floor(math.log10(number) / 3)    
-        if index > 0 and index < #prefixes then
-            return (string.gsub(string.format("%.3f%s",  number / 10^(index * 3), prefixes[index]), "%.?0+(.)$","%1"))
+function Benchmarker:toReadable(...)
+    local returns = {}
+    for _, number in ipairs({...}) do
+        if isNumber(number) then
+            local index = math.floor(math.log10(number) / 3)    
+            if self.convUnits and index > -10 and index < 9 then
+                table.insert(returns, (string.gsub(string.format("%." .. self.precision .. "f",  number / 10 ^ (index * 3)), "%.?0+$","") .. (prefixes[index] or "")))
+            else
+                table.insert(returns, string.format("%g", number))
+            end
+        else
+            error("Wrong arguments given, you can give only numbers!")
         end
     end
-    return number
+    return unpack(returns)
 end
 
-
-return Benchmarker.new()
+return Benchmarker.new() -- set default config
