eb68f1282db7d5f8e477a4fa07aa760f53204da5
VerdommeMan <=>
2021-01-22 01:29:54 +0100

    add type checking and fix areFunctions

--- src/module/init.lua
+++ src/module/init.lua
@@ -9,8 +9,25 @@ local function isFunction(arg)
     return type(arg) == "function"
 end
 
+local function areFunctions(...)
+    local args = {...}
+    local n = select("#", ...)
+
+    for i = 1, n do
+        if not isFunction(args[i]) then
+            return false
+        end
+    end
+    return true
+end
+
+-- these are modified to allow string-number coersion
 local function isNumber(arg)
-    return type(arg) == "number"
+    return tonumber(arg)
+end
+
+local function isInt(arg)
+    return isNumber(arg) and tonumber(arg) == math.floor(arg)
 end
 
 local function getPercentage(old, new)
@@ -18,40 +35,51 @@ local function getPercentage(old, new)
 end
 
 function Benchmarker.new(cycles, duration, showProgress, showFullInfo, ReadableNumbers, noYieldTime) -- set the configuration
+     -- set defaults
+    duration = duration or 1
+    cycles = cycles or 1e3
+    showFullInfo = showFullInfo == nil and true or showFullInfo -- defaults to true
+    ReadableNumbers = ReadableNumbers == nil and readableNumbers or ReadableNumbers 
+    noYieldTime = noYieldTime or 0.1
+    -- type checking
+    assert(isInt(cycles), "Wrong argument given for cycles, expected integer but received " .. typeof(cycles))
+    assert(isNumber(duration), "wrong argument given for duration, expected a number but received " .. typeof(duration))
+    assert(readableNumbers == false or readableNumbers.format, "Wrong argument given for readableNumbers, expected false/ReadableNumbers object but received " .. typeof(readableNumbers))
+    assert(isNumber(noYieldTime), "Wrong argument given for noYieldTime, expected number but received " .. typeof(noYieldTime))
+
     return setmetatable({
-        duration = duration or 1,
-        cycles = cycles or 1e3,
+        duration = duration,
+        cycles = cycles,
         showProgress = showProgress,
-        showFullInfo = showFullInfo == nil and true or showFullInfo,
-        readableNumbers = ReadableNumbers == nil and readableNumbers or ReadableNumbers ,
-        noYieldTime = noYieldTime or 0.1,
+        showFullInfo = showFullInfo,
+        readableNumbers = ReadableNumbers,
+        noYieldTime = noYieldTime
     }, Benchmarker)
 end
 
 function Benchmarker:compare(func1, func2, funcion1AmountArgs, ...)
-    if isFunction(func1) and isFunction(func2) then
+    assert(areFunctions(func1, func2), "Wrong argument given for func1/func2, expected functions but received " .. typeof(func1) .. " and " .. typeof(func2))
+    assert(isInt(funcion1AmountArgs), "Wrong argument given for funcion1AmountArgs, expected integer but received " .. typeof(func1)  )
 
-        self:print(("Performing a comparison between function1 and function2 with %s cycles and a duration of %ss"):format(self:toReadable(self.cycles, self.duration)))
-        local avg1, totalTime1 = self:getAvg(func1, unpack({...}, 1, funcion1AmountArgs) )
-        self:print(("Function1 has an average of %ss per cycle and took in total %ss"):format(self:toReadable(avg1, totalTime1)))
-        local avg2, totalTime2 = self:getAvg(func2, select((funcion1AmountArgs or 0) + 1, ...))
-        self:print(("Function2 has an average of %ss per cycle and took in total %ss"):format(self:toReadable(avg2, totalTime2)))
-        local p = getPercentage(avg1, avg2)
-        print(("Function1 average cycle is %.2f%% %s than function2!"):format(p, p < 0 and "slower" or "faster"))
-        
-        local totalAmount1, _, amountPerS1 = self:getCycles(func1, ...)
-        self:print(("function1 was called %s times (%s/s)"):format(self:toReadable(totalAmount1, amountPerS1)))
-        local totalAmount2, _, amountPerS2 = self:getCycles(func2, ...)
-        self:print(("function2 was called %s times (%s/s)"):format(self:toReadable(totalAmount2, amountPerS2)))
-        local p2 = getPercentage( amountPerS2, amountPerS1)
-        print(("Function1 has %.2f%% %s cycles/s than function2!"):format(p2, p2 < 0 and "less" or "more"))
-    else
-        error("Wrong paramaters given!")
-    end
+    self:print(("Performing a comparison between function1 and function2 with %s cycles and a duration of %ss"):format(self:toReadable(self.cycles, self.duration)))
+    local avg1, totalTime1 = self:getAvg(func1, unpack({...}, 1, funcion1AmountArgs) )
+    self:print(("Function1 has an average of %ss per cycle and took in total %ss"):format(self:toReadable(avg1, totalTime1)))
+    local avg2, totalTime2 = self:getAvg(func2, select((funcion1AmountArgs or 0) + 1, ...))
+    self:print(("Function2 has an average of %ss per cycle and took in total %ss"):format(self:toReadable(avg2, totalTime2)))
+    local p = getPercentage(avg1, avg2)
+    print(("Function1 average cycle is %.2f%% %s than function2!"):format(p, p < 0 and "slower" or "faster"))
+    
+    local totalAmount1, _, amountPerS1 = self:getCycles(func1, ...)
+    self:print(("function1 was called %s times (%s/s)"):format(self:toReadable(totalAmount1, amountPerS1)))
+    local totalAmount2, _, amountPerS2 = self:getCycles(func2, ...)
+    self:print(("function2 was called %s times (%s/s)"):format(self:toReadable(totalAmount2, amountPerS2)))
+    local p2 = getPercentage( amountPerS2, amountPerS1)
+    print(("Function1 has %.2f%% %s cycles/s than function2!"):format(p2, p2 < 0 and "less" or "more"))
 end
 
 function Benchmarker:getAvg(func, ...)
-    assert(isFunction(func), "Wrong parameters given")
+    assert(isFunction(func), "Wrong argument given for func, expected function but received " .. typeof(func))
+
     local amount = 0
     local totalTime = 0
 
@@ -79,7 +107,8 @@ function Benchmarker:getAvg(func, ...)
 end
 
 function Benchmarker:getCycles(func, ...) --need better name, gets the the cycles for the set duration
-    assert(isFunction(func), "Wrong parameters given")
+    assert(isFunction(func), "Wrong argument given for func, expected function but received " .. typeof(func))
+
     local amount = 0
     local totalTime = 0
 
@@ -107,13 +136,11 @@ function Benchmarker:getCycles(func, ...) --need better name, gets the the cycle
 end
 
 function Benchmarker:benchmark(func, ...)
-    if isFunction(func) then
-        self:print(("Performing a benchmark with %s cycles and a duration of %ss"):format(self:toReadable(self.cycles, self.duration)))
-        print(("The function took on average: %ss and took in total: %ss"):format(self:toReadable(self:getAvg(func, ...))))
-        print(("The function was called %s times in %ss (%s/s)"):format(self:toReadable(self:getCycles(func, ...))))
-    else
-        error("Wrong parameters given")
-    end  
+    assert(isFunction(func), "Wrong argument given for func, expected function but received " .. typeof(func))
+
+    self:print(("Performing a benchmark with %s cycles and a duration of %ss"):format(self:toReadable(self.cycles, self.duration)))
+    print(("The function took on average: %ss and took in total: %ss"):format(self:toReadable(self:getAvg(func, ...))))
+    print(("The function was called %s times in %ss (%s/s)"):format(self:toReadable(self:getCycles(func, ...))))
 end
 
 function Benchmarker:print(...)
